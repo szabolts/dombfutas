6df5b5f8ec0bcf88cea8568a17c0a47224b72166
szabolts <hogyeszabit@gmail.com>
2023-07-11 03:54:38 +0200

    lofasz

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
-	import { enhance } from '$app/forms'
+	import { enhance } from "$app/forms";
+	import Radio from "/home/szabikaah/Desktop/dombfutas/src/components/Radio.svelte";
+	import {
+		DateInput,
+		DatePicker,
+		localeFromDateFnsLocale,
+	} from "date-picker-svelte";
+	import { Datepicker } from "flowbite-svelte";
 
-	export let form
+	let date = new Date();
+
+	let radioValue;
+	const options = [
+		{
+			value: "Pink",
+			label: "Female",
+		},
+		{
+			value: "Blue",
+			label: "Male",
+		},
+	];
+
+	export let form;
 </script>
 
-<div class="text-column" id="div">
+<div>
 	<h1>Register</h1>
 
-	<form action="?/register" method="POST" use:enhance>
+	<form action="?/register" method="POST">
 		<div>
-			<label for="email">Email</label>
-			<input id="email" name="email" type="text" required />
+			<label for="email"></label>
+			<input id="email" name="" type="text" required />
 		</div>
 
 		<div>
@@ -18,17 +39,28 @@
 			<input id="password" name="password" type="password" required />
 		</div>
 
-        <div>
+		<div>
 			<label for="password2">Password again</label>
 			<input id="password2" name="password2" type="password" required />
 		</div>
 
-        <div>
+		<div>
 			<label for="name">Full name</label>
 			<input id="name" name="name" type="text" required />
 		</div>
 
-        
+		<div>
+			<Datepicker />
+		</div>
+
+		<div>
+			<Radio
+				{options}
+				fontSize={16}
+				legend="Select a starter Pokemon"
+				bind:userSelected={radioValue}
+			/>
+		</div>
 
 		{#if form?.user}
 			<p class="error">Email is taken.</p>
